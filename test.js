a09107421e9a01ed6cce183e587d036dbd3ac24d
Ilya Buzunov <46969838+ZTex275@users.noreply.github.com>
2019-02-27 15:55:58 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -1,26 +1,29 @@
 var game = {
-  init: function(){
- 
-  var matrix = [
-    ["-","-","-"],
-    ["-","-","-"],
-    ["-","-","-"]
-  ];
-  var j,k;
+	init: function(a){
+			this.field = a
+			game.start()
+		}
+	};
+		
+	game.start = function() {
+		var j, k;
+		matrix = game.getMassive(this.field)
+    console.log(matrix.join('\n'));
+
 
     for(i=2; i<=10; i++){
       if (i % 2 == 0){
         j = prompt ("Ход крестиков! Введите первую координату: ")
         k = prompt ("Ход крестиков! Введите вторую координату: ")
         matrix[j][k] = "x";
-        console.log (matrix[0][0] + " " + matrix[0][1] + " " + matrix[0][2] + "\n" + matrix[1][0] + " " + matrix[1][1] + " " + matrix[1][2] + "\n" + matrix[2][0] + " " + matrix[2][1] + " " + matrix[2][2])
-  }
-  else{
-    j = prompt ("Ход ноликов! Введите первую координату: ")
-    k = prompt ("Ход ноликов! Введите вторую координату: ")
-    matrix[j][k] = "o";
-    console.log (matrix[0][0] + " " + matrix[0][1] + " " + matrix[0][2] + "\n" + matrix[1][0] + " " + matrix[1][1] + " " + matrix[1][2] + "\n" + matrix[2][0] + " " + matrix[2][1] + " " + matrix[2][2])
-    }
+        console.log(matrix.join('\n'));
+      }
+      else{
+        j = prompt ("Ход ноликов! Введите первую координату: ")
+        k = prompt ("Ход ноликов! Введите вторую координату: ")
+        matrix[j][k] = "o";
+        console.log(matrix.join('\n'));
+      }
 
     if (((matrix[0][0]=="x")&&(matrix[0][1]=="x")&&(matrix[0][2]=="x"))||((matrix[1][0]=="x")&&(matrix[1][1]=="x")&&(matrix[1][2]=="x"))||((matrix[2][0]=="x")&&(matrix[2][1]=="x")&&(matrix[2][2]=="x"))||((matrix[0][0]=="x")&&(matrix[1][1]=="x")&&(matrix[2][2]=="x"))||((matrix[0][2]=="x")&&(matrix[1][1]=="x")&&(matrix[2][0]=="x"))||((matrix[0][1]=="x")&&(matrix[1][1]=="x")&&(matrix[2][1]=="x"))||((matrix[0][0]=="x")&&(matrix[1][0]=="x")&&(matrix[2][0]=="x"))){
         alert("Победа крестиков!");
@@ -29,7 +32,17 @@ var game = {
     if (((matrix[0][0]=="o")&&(matrix[0][1]=="o")&&(matrix[0][2]=="o"))||((matrix[1][0]=="o")&&(matrix[1][1]=="o")&&(matrix[1][2]=="o"))||((matrix[2][0]=="o")&&(matrix[2][1]=="o")&&(matrix[2][2]=="o"))||((matrix[0][0]=="o")&&(matrix[1][1]=="o")&&(matrix[2][2]=="o"))||((matrix[0][2]=="o")&&(matrix[1][1]=="o")&&(matrix[2][0]=="o"))||((matrix[0][1]=="o")&&(matrix[1][1]=="o")&&(matrix[2][1]=="o"))||((matrix[0][0]=="o")&&(matrix[1][0]=="o")&&(matrix[2][0]=="o"))){
         alert("Победа ноликов!");  
         break;}
-      }
     }
-  }  
-game.init()
+  };
+   
+    game.getMassive = function(){
+      var matrix = [];
+        for(var j = 0; j < this.field; j++) {
+              matrix[j] = [];
+            for(var k = 0; k < this.field; k ++) 
+              matrix[j].push("-"); 
+          }
+        return matrix;
+      };
+
+game.init(10)
